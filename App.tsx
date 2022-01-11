0a769a151cc5c17386c3b6dc1c40f6b5ca552b35
Domingos Capitango <p.capitango65@gmail.com>
2022-01-11 03:52:44 +0100

    :sparkles: add bottomsheet

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useMemo, useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { useState } from "react";
 
 export default function App() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const [start, setStart] = useState(1);
+  const [end, setEnd] = useState("50%");
+  const snapPoints = useMemo(() => [`${start}, ${end}%`], []);
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  function openModal() {
+    setStart(100);
+    setEnd("50%");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={openModal}>
+        <Text>Open</Text>
+      </TouchableOpacity>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={[start, end]}
+        backgroundStyle={styles.bottomsheetStyle}
+        // snapPoints={snapPoints}
+        onChange={handleSheetChanges}>
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
     </View>
   );
 }
@@ -13,8 +42,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: "red",
+    alignItems: "center",
+    paddingTop: 200,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  bottomsheetStyle: {
+    backgroundColor: "white",
   },
 });
